493c2f522edc7a32e1adc0f158a8e7f825c8bc0d
Vedant Singh <inboxvedantsingh@gmail.com>
2026-09-26 03:04:29 +0530

    feat(part-b): export replication manager

--- replication/__init__.py
+++ replication/__init__.py
@@ -1,6 +1,13 @@
 """Replication and storage-node control-plane services."""
 
 from .contract import CANONICAL_STORAGE_NODE_CONTRACT
+from .manager import ReplicationManager, ReplicationPolicy, ReplicationResult
 from .node_client import StorageNodeClient
 
-__all__ = ["CANONICAL_STORAGE_NODE_CONTRACT", "StorageNodeClient"]
+__all__ = [
+    "CANONICAL_STORAGE_NODE_CONTRACT",
+    "ReplicationManager",
+    "ReplicationPolicy",
+    "ReplicationResult",
+    "StorageNodeClient",
+]
