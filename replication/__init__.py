bab2a1024fbc06a0b18e260e66a65097d76b2a9a
VK-227 <vishal07.kumar222007@gmail.com>
2026-09-26 02:41:07 +0530

    integrate(control): add replication/__init__.py

---
+++ replication/__init__.py
@@ -0,0 +1,6 @@
+"""Replication and storage-node control-plane services."""
+
+from .contract import CANONICAL_STORAGE_NODE_CONTRACT
+from .node_client import StorageNodeClient
+
+__all__ = ["CANONICAL_STORAGE_NODE_CONTRACT", "StorageNodeClient"]
