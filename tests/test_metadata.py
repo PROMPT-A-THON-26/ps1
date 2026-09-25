6aab1af0c965354a3797367a1f974af8fdadcff9
Vedant Singh <inboxvedantsingh@gmail.com>
2026-09-26 02:14:25 +0530

    chore(test): remove obsolete metadata-test import

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 from datetime import datetime, timedelta, timezone
-from uuid import uuid4
-
 import pytest
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
