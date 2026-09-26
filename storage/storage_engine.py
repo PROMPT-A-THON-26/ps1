ddedb649d3163b2eacb4c1b90a62fb62b7529a8e
Vedant Singh <inboxvedantsingh@gmail.com>
2026-09-26 07:58:13 +0530

    refactor(storage): make stream cleanup exception-safe

--- storage/storage_engine.py
+++ storage/storage_engine.py
@@ -125,6 +125,8 @@ class StorageEngine:
         object_digest = hashlib.sha256()
         chunk_digest = hashlib.sha256()
         reserved_bytes = 0
+        published = False
+        completed = False
 
         try:
             async for incoming in chunks:
@@ -187,25 +189,30 @@ class StorageEngine:
             }
             self._write_metadata(staging_dir, metadata)
             self._publish_staging(object_id, version_id, staging_dir)
+            published = True
 
             with self._lock:
                 self._release_capacity(reserved_bytes)
+                reserved_bytes = 0
                 self._used_bytes += size
                 self._inflight_objects.discard((object_id, version_id))
-            reserved_bytes = 0
+            completed = True
             return size
-        except Exception:
+        finally:
             if chunk_handle is not None:
                 try:
                     chunk_handle.close()
                 except OSError:
                     pass
-            self._remove_tree(staging_dir)
+
+            if not published:
+                self._remove_tree(staging_dir)
 
             with self._lock:
-                self._release_capacity(reserved_bytes)
-                self._inflight_objects.discard((object_id, version_id))
-            raise
+                if reserved_bytes:
+                    self._release_capacity(reserved_bytes)
+                if not completed:
+                    self._inflight_objects.discard((object_id, version_id))
 
     def read_bytes(self, object_id: str, version_id: str) -> bytes:
         return b"".join(self.iter_chunks(object_id, version_id))
